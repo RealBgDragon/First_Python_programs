6a1f27926653bcdd521dc8bf174ed5b771807fc2
RealBgDragon <martin06200508@gmail.com>
2023-05-02 23:31:06 +0300

    Update wartunder.py

--- wartunder.py
+++ wartunder.py
@@ -1,11 +1,32 @@
-print("Range")
-x = 1
-while x!=0:
-    x = int(input("Enter a number: "))
+import tkinter as tk
 
+window = tk.Tk()
+
+window.geometry("500x600")
+
+label = tk.Label(window, width=30, font=("Ariel", 20), text="Wartunder calculator")
+label.pack()
+
+entry_wiget_1 = tk.Entry(window, width=30, font=("Ariel", 20))
+entry_wiget_1.place(x=250, y=50, anchor=tk.CENTER)
+   
+label_ans = []
+
+for i in range(9):
+    ans_label = tk.Label(window, width=20, font=("Ariel", 20))
+    ans_label.place(x=250, y=150 + 50*i, anchor=tk.CENTER)
+    label_ans.append(ans_label)
+
+x = 1
+
+def calcualte():
+    x = int(entry_wiget_1.get())
+    
     for i in range(1, 10):
         result = x * i
-        print("За ", i, " ", result)
+        label_ans[i-1].config(text="За {}: {}".format(i, result))
+
+calcualte_button = tk.Button(window, text="Find range", font=("Ariel", 20), command=calcualte)
+calcualte_button.place(x=250, y=100, anchor=tk.CENTER)
     
-    
-    
+window.mainloop()
