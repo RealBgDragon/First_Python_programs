ad87d09c8cf1dbe0fc1c80aeb0777207212c626a
RealBgDragon <martin06200508@gmail.com>
2023-05-03 00:15:09 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,42 +1,75 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+window = Tk()
 
-window.geometry("500x500")
-
-label = tk.Label(window, text="Calculator")
+label = Label(window, width=20, font=("Ariel", 16), text="To-Do list")
 label.pack()
 
-entry_widget1 = tk.Entry(window, width=30, font=("Ariel", 20))
-entry_widget1.place(x=250, y=50, anchor=tk.CENTER)
+window.geometry("500x750")
 
-entry_widget2 = tk.Entry(window, width=30, font=("Ariel", 20))
-entry_widget2.place(x=250,y=125, anchor=tk.CENTER)
+task_list = Listbox(window)
+task_list.pack(side=RIGHT, fill=BOTH, expand=TRUE)
 
-label1 = tk.Label(window, font=("Ariel", 20))
-label1.place(x=250, y=450, anchor=tk.CENTER)
+#task entry
+task_entry = Entry(window)
+task_entry.pack(side=LEFT)
 
-x = 0
-y = 0
-def add():
-    x = float(entry_widget1.get())
-    y = float(entry_widget2.get())
-    ans = x + y
-    label1.configure(text=str(ans))
+
+scrollbar = Scrollbar(window)
+scrollbar.pack(side=RIGHT, fill=Y)
+task_list.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=task_list.yview)
+
+menu = Menu(window)
+window.config(menu=menu)
+
+#add_button methood
+
+def add_task():
+    task = task_entry.get()
+    #insert into the end
+    task_list.insert(END, task)
+    #clear the widget
+    task_entry.delete(0, END)
+
+# task entry button
+add_button = Button(window, text="Add task", command=add_task)
+add_button.pack(side=LEFT)
+
+
+
+
+
+file_menu = Menu(menu, font=("Arial", 10))
+
+
+
+
+#* exit the program
+def exit_program():
+    window.destroy()
     
-add_button = tk.Button(window, text="+", width=5, height=1, command=add)
-add_button.configure(font=("Ariel", 20))
-add_button.place(x=25, y=200)
 
-def sub():
-    x = float(entry_widget1.get())
-    y = float(entry_widget2.get())
-    ans = x - y
-    label1.configure(text=str(ans))
+def new_task_list():
+    print("New Task List")
 
-sub_button = tk.Button(window, text="-", width=5, height=1, command=sub)
-sub_button.configure(font=("Ariel", 20))
-sub_button.place(x = 125, y = 200)
+def open_task_list():
+    print("Open Task List")
+
+def save_task_list():
+    print("Save Task List")
+    
+    
+#! add a functionallity to the options
+file_menu.add_command(label="New Task List", command=new_task_list)
+file_menu.add_command(label="Open Task List", command=new_task_list)
+file_menu.add_command(label="Save Task List", command=new_task_list)
+
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=exit_program)
+menu.add_cascade(label="File", menu=file_menu)
+
+
 
 
 window.mainloop()
