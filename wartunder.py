ec6b2290faa5df89fb91d04a7be0c0f4e3e3c8c1
RealBgDragon <martin06200508@gmail.com>
2023-05-02 15:22:26 +0300

    Create wartunder.py

---
+++ wartunder.py
@@ -0,0 +1,11 @@
+print("Range")
+x = 1
+while x!=0:
+    x = int(input("Enter a number: "))
+
+    for i in range(1, 10):
+        result = x * i
+        print("За ", i, " ", result)
+    
+    
+    
